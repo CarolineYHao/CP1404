dbcf3fb4d953585ae0250197612c7eab6e645dfe
cuddlelion <cuddlelion@users.noreply.github.com>
2018-08-20 21:39:38 +1000

    Refactor password program to use functions

--- Prac 3/passwor_checker.py
+++ Prac 3/passwor_checker.py
@@ -1,6 +1,15 @@
-min_length = int(input("Enter minimum length required: "))
-password = input("Enter your password: ")
-while len(password) < min_length:
-    print("Password needs to be at least {} characters.".format(min_length))
+def main():
+    min_length = int(input("Enter minimum length required: "))
+    password = get_password(min_length)
+    print("*" * len(password))
+
+
+def get_password(min_length):
     password = input("Enter your password: ")
-print("*" * len(password))
+    while len(password) < min_length:
+        print("Password needs to be at least {} characters.".format(min_length))
+        password = input("Enter your password: ")
+    return password
+
+
+main()
