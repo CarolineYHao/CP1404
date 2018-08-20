c1845de773b93eb51d372d136367631acf263647
cuddlelion <cuddlelion@users.noreply.github.com>
2018-08-20 22:07:51 +1000

    Add format checking

--- Prac 3/word_generator.py
+++ Prac 3/word_generator.py
@@ -6,16 +6,31 @@ Another way to get just consonants would be to use string.ascii_lowercase
 (all letters) and remove the vowels.
 """
 import random
-
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-word_format = input("Enter word pattern where 'c' is a consonant and 'v' is a vowel. (eg. ccvcvvc): \n").lower()
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
 
-print(word)
+def main():
+    word_format = input("Enter word pattern where 'c' is a consonant and 'v' is a vowel. (eg. ccvcvvc): \n").lower()
+    is_valid_format(word_format)
+    while is_valid_format(word_format) is False:
+        print("Only 'c' and 'v' are allowed")
+        word_format = input("Enter word pattern where 'c' is a consonant and 'v' is a vowel. (eg. ccvcvvc): \n").lower()
+    word = ""
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+    print(word)
+
+
+def is_valid_format(word_format):
+    for char in word_format:
+        if char in 'cv':
+            return True
+        else:
+            return False
+
+
+main()
