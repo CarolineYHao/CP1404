531ee4a851ec874ec5a3e3f76069baa614ab88a7
cuddlelion <cuddlelion@users.noreply.github.com>
2018-08-20 21:36:49 +1000

    Add password checker program

---
+++ Prac 3/passwor_checker.py
@@ -0,0 +1,6 @@
+min_length = int(input("Enter minimum length required: "))
+password = input("Enter your password: ")
+while len(password) < min_length:
+    print("Password needs to be at least {} characters.".format(min_length))
+    password = input("Enter your password: ")
+print("*" * len(password))
